015522a6057603cce8f50417c07f2b7ba061b59a
Carlos Rodriguez <Carlos Rodriguez>
2017-06-27 02:31:09 +0100

    Modal window menu for difficulty, (not working yet)

--- public/js/puzzlePage.js
+++ public/js/puzzlePage.js
@@ -1,5 +1,5 @@
 //This should be optional
-const PUZZLE_DIFFICULTY = 5;
+const PUZZLE_DIFFICULTY = 4;
 const PUZZLE_HOVER_TINT = '#009900';
 
 var _canvas;
@@ -15,6 +15,39 @@ var _currentPiece;
 var _currentDropPiece;
 
 var _mouse;
+
+//Modal Window:
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onload = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 function init() {
     _img = new Image();
